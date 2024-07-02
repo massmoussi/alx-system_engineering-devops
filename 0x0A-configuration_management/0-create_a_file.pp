83335ea28f46846e3416e06598410c97c81fec7f
massmoussi <assermouh.abdelhak2019@gmail.com>
2024-07-02 08:00:41 -0700

    install flask

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Craete a file
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
+
---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,5 @@
+# Install Flask Version 2.1.0
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
