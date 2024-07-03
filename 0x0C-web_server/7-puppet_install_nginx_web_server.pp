a25351c01118879880a9e641ce9605892a1cf263
killua <killua@killua2.0>
2024-07-03 22:27:53 +0100

    7f

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -13,3 +13,13 @@ file { '/var/www/html/index.html':
   content => 'Hello World!',
   require => Package['nginx'],
 }
+
+exec {'redirect_me':
+  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell'
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+
